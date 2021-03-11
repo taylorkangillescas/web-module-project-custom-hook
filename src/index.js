5fe431de45dcd8282b9ad5356e4798a005b95b44
Taylor Kang-Illescas <taylorkangillescas@Taylors-MacBook-Pro-8.local>
2021-03-10 23:55:58 -0800

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-
+//test
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 
